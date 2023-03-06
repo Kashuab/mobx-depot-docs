cfd539b56bc20d40b91c28eb5fee4b7e1c15c120
Kyle Kashuba <kyle+github@kashubadev.com>
2023-03-05 17:37:16 -0800

    Added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,28 @@ import type { AppProps } from 'next/app'
 import {Box, ChakraProvider} from "@chakra-ui/react";
 import {NavigationBar} from "~/components/NavigationBar";
 import {theme} from "~/theme/theme";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import './styles/index.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <NavigationBar />
+    <>
+      <GoogleAnalytics />
+      <ChakraProvider theme={theme}>
+        <NavigationBar />
 
-      <Box
-        backgroundColor="gray.900"
-        width="100%"
-        minHeight="100vh"
-        display="flex"
-        flexDirection="column"
-        maxWidth="100%"
-      >
-        <Component {...pageProps} />
-      </Box>
-    </ChakraProvider>
+        <Box
+          backgroundColor="gray.900"
+          width="100%"
+          minHeight="100vh"
+          display="flex"
+          flexDirection="column"
+          maxWidth="100%"
+        >
+          <Component {...pageProps} />
+        </Box>
+      </ChakraProvider>
+    </>
   )
 }
